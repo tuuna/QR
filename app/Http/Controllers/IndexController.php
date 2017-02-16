ed2d649a2f579c092948696962a52b87529e08cc
vampirebitter <lj550566181@gmail.com>
2017-02-16 22:58:33 +0800

    complete

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,18 @@ class IndexController extends Controller
 {
         public function index()
         {
+            $ip = $this->getIp();
             $uploads = Upload::all();
-            return view('index',['uploads' => $uploads]);
+            return view('index',['uploads' => $uploads,'ip' => $ip]);
+        }
+
+        public function getIp()
+        {
+            $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+            socket_connect($sock, "8.8.8.8", 53);
+            socket_getsockname($sock, $name); // $name passed by reference
+
+            $localAddr = $name;
+            return $localAddr;
         }
 }
